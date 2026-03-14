fdd06cf64315d89b6c0924d537500c02ca27a7f5
abhays07 <itsrajputabhaysingh997@gmail.com>
2026-03-14 10:34:46 +0530

    [Abhay Singh Lodhi] : Initial setup

---
+++ AutomationTestingProject/src/main/java/com/project/application/ApplicationMain.java
@@ -0,0 +1,10 @@
+package com.project.application;
+
+public class ApplicationMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ AutomationTestingProject/src/test/java/com/project/application/ApplicationMainTest.java
@@ -0,0 +1,10 @@
+package com.project.application;
+
+public class ApplicationMainTest {
+
+	public static void main(String[] args) {
+		System.out.println("This is main Test File..");
+
+	}
+
+}
